07c9d9aac4b2f0e89cfcbdcd0422c4a26ee2d841
wubin666 <3239987006@qq.com>
2019-03-26 16:38:28 +0800

    定时器按钮

--- 10-timer-button/main.js
+++ 10-timer-button/main.js
@@ -1,3 +1,17 @@
 $(function(){
-  console.log('hello world!');
+  var n=6;
+  var $btnAgree=$('input[type="button"]');
+  var timer=window.setInterval(function(){
+    n--;
+    if(n===0){
+      window.clearInterval(timer);
+      $btnAgree.removeAttr('disabled');
+      $btnAgree.val('统一');
+    }else{
+      $btnAgree.val('统一('+n+'s)');
+    }
+  },1000);
+  $btnAgree.click(function(){
+    alert('SPA! 10-timer-button task!');
+  });
 });
